d605f7997e5e5bafb8a61aae4aeb9aeed52705d9
sennewin <senne.winters@hotmail.nl>
2024-06-10 12:49:54 +0200

    add imports

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Code goes here
+# Importing librariespi
+import pandas as pd
+import rdkit as rd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as snsco
+import sklearn as sk
+
+
