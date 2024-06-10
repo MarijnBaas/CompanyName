50560cb04b273b455e89c1b5fa65ffc88b549356
Gijsbert_a <gijsbert.aarts4@gmail.com>
2024-06-10 12:57:06 +0200

    Inlezen moleculen
    Uit het bestand van Wouter

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # Importing librariespi
 import pandas as pd
-import rdkit as rd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as snsco
+import seaborn as sns
 import sklearn as sk
+from rdkit import Chem
+from rdkit.Chem import Draw
 
+file = 'tested_molecules.csv'
+molecule_data = pd.read_csv(file)
+
+smiles_list = molecule_data['SMILES'].tolist()
+
+molecules = [Chem.MolFromSmiles(smiles) for smiles in smiles_list[:5]]
+
+plt.figure(figsize=(100, 100))
+for i, mol in enumerate(molecules):
+    plt.subplot(1, len(molecules), i + 1)
+    img = Draw.MolToImage(mol)
+    plt.imshow(img)
+    plt.axis('off')
+
+plt.show()
 
