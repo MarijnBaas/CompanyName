b51634e2f6c918bbf1a1004abce9a8bbcf7713b7
sennewin <senne.winters@hotmail.nl>
2024-06-10 13:16:34 +0200

    Refactor main.py for improved readability and modularity

--- main.py
+++ main.py
@@ -7,19 +7,38 @@ import sklearn as sk
 from rdkit import Chem
 from rdkit.Chem import Draw
 
+def read_data(file):
+    """
+    Reads the data from the file
+    :param file: file to read
+    :return: pandas dataframe
+    """
+    return pd.read_csv(file)
+
+
+def draw_molecule(molecule_data):
+    """
+    Draws the molecules
+    :param molecule_data: pandas dataframe
+    :return: None
+    """
+    smiles_list = molecule_data['SMILES'].tolist()
+
+    molecules = [Chem.MolFromSmiles(smiles) for smiles in smiles_list[:5]]
+
+    plt.figure(figsize=(100, 100))
+    for i, mol in enumerate(molecules):
+        plt.subplot(1, len(molecules), i + 1)
+        img = Draw.MolToImage(mol)
+        plt.imshow(img)
+        plt.axis('off')
+
+    plt.show()
+
+
+# Example
 file = 'tested_molecules.csv'
-molecule_data = pd.read_csv(file)
+df = read_data('tested_molecules.csv')
 
-smiles_list = molecule_data['SMILES'].tolist()
-
-molecules = [Chem.MolFromSmiles(smiles) for smiles in smiles_list[:5]]
-
-plt.figure(figsize=(100, 100))
-for i, mol in enumerate(molecules):
-    plt.subplot(1, len(molecules), i + 1)
-    img = Draw.MolToImage(mol)
-    plt.imshow(img)
-    plt.axis('off')
-
-plt.show()
+draw_molecule(df)
 
