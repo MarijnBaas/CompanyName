b106c450322176d3e9a5aed33693f9b2c3cec8ec
sennewin <senne.winters@hotmail.nl>
2024-06-12 15:54:57 +0200

    Add additional molecule properties

--- machine_learning_senne.py
+++ machine_learning_senne.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from rdkit import Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, Descriptors, rdMolDescriptors
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 # Load the dataset
 data = pd.read_csv('tested_molecules.csv')
@@ -10,17 +12,43 @@ data = pd.read_csv('tested_molecules.csv')
 # Function to convert SMILES to different fingerprints
 def smiles_to_fingerprints(smiles, radius=2, nBits=1024):
     molecule = Chem.MolFromSmiles(smiles)
-    
     return AllChem.GetMorganFingerprintAsBitVect(molecule, radius, nBits)
 
-data['Fingerprints'] = data['SMILES'].apply(lambda x: smiles_to_fingerprints(x).ToBitString())
+# Function to compute physicochemical properties
+def compute_properties(smiles):
+    molecule = Chem.MolFromSmiles(smiles)
+    properties = {
+        'MolWt': Descriptors.MolWt(molecule),
+        'MolLogP': Descriptors.MolLogP(molecule),
+        'NumHDonors': Descriptors.NumHDonors(molecule),
+        'NumHAcceptors': Descriptors.NumHAcceptors(molecule),
+        'TPSA': Descriptors.TPSA(molecule)
+    }
+    return properties
 
-# Filter out any molecules that could not be processed
+# Function to compute pharmacophore features
+def compute_pharmacophore(smiles):
+    molecule = Chem.MolFromSmiles(smiles)
+    pharmacophore = {
+        'NumAromaticRings': rdMolDescriptors.CalcNumAromaticRings(molecule),
+        'NumSaturatedRings': rdMolDescriptors.CalcNumSaturatedRings(molecule),
+        'NumRotatableBonds': Descriptors.NumRotatableBonds(molecule),
+        'NumHeteroatoms': Descriptors.NumHeteroatoms(molecule)
+    }
+    return pharmacophore
+
+# Apply the functions to the data
+data['Fingerprints'] = data['SMILES'].apply(lambda x: smiles_to_fingerprints(x).ToBitString())
 data = data.dropna(subset=['Fingerprints'])
 
-# Convert fingerprints to a DataFrame
 fingerprints_df = data['Fingerprints'].apply(lambda x: pd.Series(list(map(int, x))))
 fingerprints_df.columns = [f'Bit_{i}' for i in range(fingerprints_df.shape[1])]
+
+properties_df = data['SMILES'].apply(lambda x: pd.Series(compute_properties(x)))
+pharmacophore_df = data['SMILES'].apply(lambda x: pd.Series(compute_pharmacophore(x)))
+
+# Combine all features
+combined_features_df = pd.concat([fingerprints_df, properties_df, pharmacophore_df], axis=1)
 
 def prepare_data(data, target):
     # Prepare feature and target variables
@@ -32,30 +60,30 @@ def prepare_data(data, target):
 
     return X_train, X_test, y_train, y_test
 
-def train_model(X_train, y_train):
-    # Train a Random Forest Regressor
-    rf = RandomForestRegressor(random_state=42)
+def train_rf_model(X_train, y_train):
+    # Train a Random Forest Classifier
+    rf = RandomForestClassifier(random_state=42)
     rf.fit(X_train, y_train)
-
     return rf
 
-X_train_PKM2, X_test_PKM2, y_train_PKM2, y_test_PKM2 = prepare_data(fingerprints_df, data['PKM2_inhibition'])
-X_train_ERK2, X_test_ERK2, y_train_ERK2, y_test_ERK2 = prepare_data(fingerprints_df, data['ERK2_inhibition'])
+X_train_PKM2, X_test_PKM2, y_train_PKM2, y_test_PKM2 = prepare_data(combined_features_df, data['PKM2_inhibition'])
+X_train_ERK2, X_test_ERK2, y_train_ERK2, y_test_ERK2 = prepare_data(combined_features_df, data['ERK2_inhibition'])
 
-rf_PKM2 = train_model(X_train_PKM2, y_train_PKM2)
-rf_ERK2 = train_model(X_train_ERK2, y_train_ERK2)
+rf_PKM2 = train_rf_model(X_train_PKM2, y_train_PKM2)
+rf_ERK2 = train_rf_model(X_train_ERK2, y_train_ERK2)
+
 
 # Predict on the test set
-y_pred_PKM2 = rf_PKM2.predict(X_test_PKM2)
-y_pred_ERK2 = rf_ERK2.predict(X_test_ERK2)
+y_pred_PKM2_rf = rf_PKM2.predict(X_test_PKM2)
+y_pred_ERK2_rf = rf_ERK2.predict(X_test_ERK2)
 
+# Calculate the accuracy for PKM2_inhibition
+accuracy_PKM2_rf = accuracy_score(y_test_PKM2, y_pred_PKM2_rf)
 
-# Calculate the number of correct predictions for PKM2_inhibition
-correct_predictions_PKM2 = sum(y_pred_PKM2 == y_test_PKM2)
+# Calculate the accuracy for ERK2_inhibition
+accuracy_ERK2_rf = accuracy_score(y_test_ERK2, y_pred_ERK2_rf)
 
-# Calculate the number of correct predictions for ERK2_inhibition
-correct_predictions_ERK2 = sum(y_pred_ERK2 == y_test_ERK2)
+# Print the accuracies
+print("Random Forest Accuracy for PKM2_inhibition:", accuracy_PKM2_rf)
 
-# Print the number of correct predictions
-print("Number of correct predictions for PKM2_inhibition:", correct_predictions_PKM2)
-print("Number of correct predictions for ERK2_inhibition:", correct_predictions_ERK2)
+print("Random Forest Accuracy for ERK2_inhibition:", accuracy_ERK2_rf)
